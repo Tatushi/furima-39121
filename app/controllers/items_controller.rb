201e6a7b5b4caf620a6ea2e6fa1b77267b4660b1
Tatushi <totti712@gmail.com>
2023-05-01 16:47:13 +0900

    edit.html作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,13 +22,13 @@ class ItemsController < ApplicationController
     end
   end
 
-#  def edit
-    # ログインしているユーザーと同一であればeditファイルが読み込まれる
-#    if @item.user_id == current_user.id && @item.order.nil?
-#    else
-#      redirect_to root_path
-#    end
-#  end
+  def edit
+
+    if @item.user_id == current_user.id && @item.order.nil?
+    else
+      redirect_to root_path
+    end
+  end
 
 #  def update
 #    @item.update(item_params)
