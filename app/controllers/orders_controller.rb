1390f1adfe90f54f0598d49eb5072c747be0ca4a
Tatushi <totti712@gmail.com>
2023-05-03 11:04:47 +0900

    index create Action定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,17 @@
 class OrdersController < ApplicationController
+
+  def index
+    @order_form = OrderForm.new
+  end
+
+  def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
 end
