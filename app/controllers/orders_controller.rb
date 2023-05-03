1d1431b7e4b21de5276bcdca4cf624015bed0b59
Tatushi <totti712@gmail.com>
2023-05-03 15:45:29 +0900

    gem導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,4 +14,12 @@ class OrdersController < ApplicationController
       render :index
     end
   end
+
+  def order_params
+    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+
+
+
 end
