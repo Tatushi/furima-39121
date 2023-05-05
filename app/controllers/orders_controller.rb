b2fa1bb298a516e10ab061fdf8cdeac46df097a4
Tatushi <totti712@gmail.com>
2023-05-05 16:29:00 +0900

    アクション記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :non_purchased_item, only: [:index, :create]
 
   def index
     @order_form = OrderForm.new
@@ -15,20 +17,24 @@ class OrdersController < ApplicationController
     end
   end
 
+  private
+
   def order_params
     params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-      amount: @item.price,        
-      card: order_params[:token], 
-      currency: 'jpy'
+      amount: @item.price,        # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy'             # 通貨の種類（日本円）
     )
-
   end
 
+  def non_purchased_item
 
-
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+  end
 end
