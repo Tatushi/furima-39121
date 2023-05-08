b0aec2439febd1c070f2a63f251b87fd97e6561f
Tatushi <totti712@gmail.com>
2023-05-08 15:45:51 +0900

    order_form修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,11 +3,11 @@ class OrdersController < ApplicationController
   before_action :non_purchased_item, only: [:index, :create]
 
   def index
-    @order_form = OrderPayment.new
+    @order_payment = OrderPayment.new
   end
 
   def create
-    @order_form = OrderPayment.new(order_params)
+    @order_payment = OrderPayment.new(order_params)
     if @order_payment.valid?
       pay_item
       @order_payment.save
@@ -20,7 +20,8 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_payment).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_payment).permit(:post_code, :prefecture_id, :city, :address, :building_name,
+      :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
