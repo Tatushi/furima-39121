be343c71f2513f8744854262c9dd35e7f3b7aec9
Tatushi <totti712@gmail.com>
2023-05-09 18:38:06 +0900

    コード修正

--- spec/models/order_payment_spec.rb
+++ spec/models/order_payment_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe OrderPayment, type: :model do
   before do
-    @order_payment = FactoryBot.build(:order_payment)
+    user=FactoryBot.create(:user)
+    item=FactoryBot.create(:item)
+    @order_payment = FactoryBot.build(:order_payment, user_id: user.id, item_id: item.id )
   end
 
   describe '配送先情報の保存' do
@@ -105,6 +107,12 @@ RSpec.describe OrderPayment, type: :model do
         @order_payment.valid?
         expect(@order_payment.errors.full_messages).to include("Token can't be blank")
       end
+
+      it '電話番号は10桁未満では登録できない' do
+        @order_payment.phone_number = '123456789'
+        @order_payment.valid?
+        expect(@order_payment.errors.full_messages).to include('Phone number is invalid')
+        end
     end
   end
 end
